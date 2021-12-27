944995bd8d274b53f52de4b7b3d7dcdb633c3f94
Glenn Marshall-Adams <glenn.marshalladams@icloud.com>
2021-12-27 16:06:14 +0000

    using @PostConstruct & @PreDestroy

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -3,11 +3,12 @@ package com.luv2code.springdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
     @Qualifier("fileFortuneService")
@@ -37,6 +38,18 @@ public class TennisCoach implements Coach {
     //define a default constructor
     public TennisCoach() {
         System.out.println("TennisCoach: Inside default constructor");
+    }
+
+    //define my init  method
+    @PostConstruct
+    public void doMyStartUpStuff() {
+        System.out.println("TennisCoach: inside of doMyStartUpStuff");
+    }
+
+    //define my destroy method
+    @PreDestroy
+    public void doMyCleanUpStuff() {
+        System.out.println("TennisCoach: inside of doMyCleanUpStuff" + fortuneService.getFortune());
     }
 
     @Override
